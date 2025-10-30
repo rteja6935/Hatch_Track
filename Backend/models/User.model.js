c7c49d59a0209597cbf926db8f7ac088fe4817c1
S Raviteja <rteja6935@gmail.com>
2025-10-30 06:22:59 +0530

    made changes in user sigup/login

--- Backend/models/User.model.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const UserSchema = new mongoose.Schema({
-  phoneNumber: { type: String, required: true, unique: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model('User', UserSchema);
---
+++ Backend/models/UserAuth.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      sparse: true, // allows multiple users without email
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      minlength: 6,
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('UserAuth', UserSchema);
